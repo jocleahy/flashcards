2c96923da8a978a44d4fccebc785e67399816629
jocleahy <leahyjuliannz@gmail.com>
2020-04-19 11:32:41 +1200

    add styling for images, change px to rems

--- js/data.js
+++ js/data.js
@@ -1,18 +1,13 @@
 export let data = [
     {
         "id": 1,
-        "front": "<h1>front 1</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p>",
+        "front": "<h1>front with img</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p><img src='images/dog.jpg'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p>",
         "back": "<h1>back 1</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p>"
     },
     {
         "id": 2,
-        "front": "<h1>front 2</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p>",
+        "front": "<h1>front 2</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p><img src='images/dog.jpg'>",
         "back": "<h1>back 2</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p>"
-    },
-    {
-        "id": 3,
-        "front": "<h1>front 3</h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p>",
-        "back": "<h2>Answer 3</h2><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate adipisci, perferendis quas eum quis eveniet aspernatur.</p><p> Iste voluptatum dolorem ea, ducimus perspiciatis aperiam ex qui ab sapiente facere nesciunt tempora!</p>"
     }
 ];
 
